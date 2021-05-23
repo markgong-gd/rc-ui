1c25e587456304185585265973bffcb96e1b881d
gongding <15908606719@163.com>
2021-05-23 13:41:50 +0800

    feat: 发布到npm

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
     },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'lib'),
+        path: path.resolve(__dirname, '../lib'),
         library: 'rc-ui',
         library: 'umd'
     },
